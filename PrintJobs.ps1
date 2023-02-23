c4722e2feac4bdd1a9387c807475850df5877129
Josh <32531221+joshuamainsmith@users.noreply.github.com>
2023-02-23 09:42:35 -0800

    Added logging

--- PrintJobs.ps1
+++ PrintJobs.ps1
@@ -1,20 +1,20 @@
 param([switch]$Elevated)
 
-function Test-Admin{
+function Test-Admin {
     $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
     $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
 
-if ((Test-Admin) -eq $false) {
+if ((Test-Admin) -eq $false)  {
     if ($elevated) {
-        # not elevated
+        # tried to elevate, did not work, aborting
     } else {
-        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($MyInvocation.MyCommand.Definition))
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
     }
     exit
 }
 
-$Printer = Read-Host "Enter printer name: "
+$PrinterName = Read-Host "Enter printer name: "
 $FirstPrintJob = -1
 
 try
@@ -22,21 +22,22 @@ try
     while ($true)
     {
         Write-Host "Checked $(Get-Date)"
-
         $s = Get-Service Spooler
         if ($s.Status -eq "Stopped")
         {
             'Spooler Stopped'
-            #Log
-            Start-Service -InputObject $s -PassThru
+            Get-Date >> C:\PrintSpoolerLog.txt
+            Start-Service -InputObject $s -PassThru | Format-List >> C:\PrintSpoolerLog.txt
         }
 
-        $PrinterJobs = Get-Printer -Name $Printer
-        $PrintJobsById = Get-PrintJob -PrinterObject $PrinterJobs | select -ExpandProperty "Id"
+        $Printer = Get-Printer -Name $PrinterName
+        $PrintJobsById = Get-PrintJob -PrinterObject $Printer | select -ExpandProperty "Id"
 
         if ($PrintJobsById[0] -eq $FirstPrintJob)
         {
-            Restart-Service -InputObject $s -PassThru
+            'Print Jobs Stuck'
+            Get-Date >> C:\PrintJobLog.txt
+            Restart-Service -InputObject $s -PassThru | Format-List >> C:\PrintJobLog.txt
         }
 
         $FirstPrintJob = $PrintJobsById[0]
@@ -46,5 +47,7 @@ try
 }
 finally
 {
-    'Done'
+    write-host "Script Stopped"
+    Get-Date >> C:\PrintScriptStoppedLog.txt
+    $Printer >> C:\PrintScriptStoppedLog.txt
 }
